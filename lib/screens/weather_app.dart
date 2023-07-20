0baf309d6fcf3bb4d677a7ffa9bbc952811c1b9c
Kenneth <kennethdsouza2004@gmail.com>
2023-07-21 01:54:41 +0530

    basic structure of the app

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -25,12 +25,15 @@ class WeatherApp extends StatelessWidget {
         ),
         actions: [
           Container(
-              margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              child: SvgPicture.asset(
-                'assets/menu.svg',
-                height: 45,
-                width: 45,
-                color: Colors.white,
+              margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: GestureDetector(
+                onTap: () => print('Menu Clicked'),
+                child: SvgPicture.asset(
+                  'assets/menu.svg',
+                  height: 45,
+                  width: 45,
+                  color: Colors.white,
+                ),
               ))
         ],
       ),
@@ -43,6 +46,99 @@ class WeatherApp extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
           ),
+          Container(
+            decoration: const BoxDecoration(color: Colors.black38),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 120,
+                          ),
+                          Text(
+                            'Kolkata',
+                            style: GoogleFonts.lato(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'hello',
+                            style: GoogleFonts.lato(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Kolkata',
+                            style: GoogleFonts.lato(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'hello',
+                            style: GoogleFonts.lato(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 40),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white30,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Kolkata',
+                          style: GoogleFonts.lato(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          'hello',
+                          style: GoogleFonts.lato(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       )),
     );
